882bb0e4316a3bd92f8e450956663f2428cfd414
Andres Seivane <andyseivane@hotmail.com>
2022-10-16 22:11:10 -0300

    V2

--- app.js
+++ app.js
@@ -5,22 +5,50 @@ https://www.youtube.com/watch?v=pk5WNnTzYyw
 
 https://www.youtube.com/watch?v=_8HdvDqMVUI
 
+
+Ejecucion de comando bash: https://stackoverflow.com/questions/44647778/how-to-run-shell-script-file-using-nodejs
+
 */
 
 
-const express = require ('express');
-const app     = express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+const shell = require('shelljs')
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 
-
-app.listen(3000, () => {
-  console.log('Servidor iniciado');
-
-  const button = document.getElementById('myButton');
-  button.addEventListener('click', function(e) {
-    console.log('button was clicked');
-  });
-
+app.get('/',function(req,res){
+res.sendfile("index.html");
 });
+
+app.post('/login',function(req,res){
+var user_name = req.body.user;
+var password = req.body.password;
+console.log("From Client POST request: User name = "+user_name+" and password is "+password);
+shell.exec('./prueba.sh')
+res.end("yes");
+});
+
+
+app.post('/consulta',function(req,res){
+console.log("Consulta de estado enviada");
+shell.exec('./generacion_tabla_nodos.sh')
+res.end("yes");
+});
+
+
+app.listen(3000,function(){
+console.log("Servidor WEB iniciado en el puerto 3000");
+})
+
+
+
+/*
+app.post('/test', function (req, res) {
+    console.log('works');
+});
+*/
---
+++ generacion_tabla_nodos.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Con openwrt debemos cambiar lo anterior por #!/bin/sh
+
+# Parametros iniciales
+
+broker="192.168.0.10"
+port="1883"
+usr="usuario"
+pass="usuariopassword"
+
+
+topic2="nodo/estado"
+
+
+
+archivo1="estado/mensajes_mqtt.log"
+archivo2="estado/tabla_nodos_inicio.csv"
+
+echo "AVERIGUANDO ESTADO DE LOS NODOS"
+
+
+# Preguntar por el estado de los nodos
+
+mosquitto_pub -t control/estado -h $broker -p $port -m "0" -u $usr -P $pass # Consulta de estado a todos los nodos
+
+while read value; do
+
+  ts=$(date "+%Y/%m/%d %H:%M:%S") # Guardamos la fecha y hora actual en una variable.
+
+   echo "Hora Actual: $ts"
+
+  topic=`echo "$value" | awk '{print $1}'`
+
+
+  if [[ $topic = $topic2 ]]; then
+  # guardar id y estado del nodo recibido
+  # PREGUNTAR SI EL NODO ESTA REPETIDO EN LA TABLA
+  nodoid=`echo "$value" | awk '{print $2 "," $3 "," $4 "," $6}'`
+  nodoid_shell=`echo "$value" | awk '{print $2 "," $3}'`
+  echo "$nodoid" >> $archivo2   # guardamos datos en archivo
+  echo "$ts mensaje recibido: [$value]" >> $archivo1   # guardamos datos en archivo
+  echo "$ts Nodo identificado: $nodoid_shell"
+  else
+  # mensaje no identificado
+  echo "$ts mensaje recibido no identificao!: [$value]" >> $archivo1   # guardamos datos en archivo
+  echo "$ts mensaje recibido no identificao!: [$value]"
+  fi
+sync=`echo "$value" | awk '{print $6}'`
+
+done < <(timeout 5s mosquitto_sub -t $topic2 -h $broker -p $port -v -u $usr -P $pass) # se pone el comando acá eb vez de antes del while porque sino se pierde el valor de las variables dentro del bucle
+echo "Fin"
