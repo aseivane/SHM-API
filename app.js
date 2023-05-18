bef0578581ce6c7e44c821372b0361ca4cb150a9
lmazzer <lucamazzer92@gmail.com>
2023-05-18 00:20:59 -0300

    fix: execution

--- app.js
+++ app.js
@@ -39,10 +39,11 @@ var serveIndex = require('serve-index');
 var zipper = require('zip-local');
 var fs = require('fs');
 var moment = require('moment')
-var ip_mqtt_broker = '192.168.100.98';
+var ip_mqtt_broker = '192.168.1.230';
 var usuario_mqtt = 'usuario';
 var pass_mqtt = 'usuariopassword';
 
+ 
 var app = express();
 const shell = require('shelljs')
 const { spawn } = require("child_process"); // Para ejecutar scripts en un proceso nuevo
@@ -65,32 +66,31 @@ res.sendfile("index.html");
 
 app.post('/form_config_sistema',function(req,res){
     console.log("Formulario completado:");
+    console.log("IP del Broker: " + req.body.ip_mqtt);
+    console.log("Usuario: " + req.body.usr_mqtt);
+    console.log("Passwd: "+ req.body.pass_mqtt);
     
     ip_mqtt_broker = req.body.ip_mqtt;
     usuario_mqtt = req.body.usr_mqtt;
     pass_mqtt = req.body.pass_mqtt;
    
-    return res.status(200);
+    return res.status(200).json({test: 'test'});
 
 });
 
 app.post('/actualizar_estados', async function(req,res){
     console.log("Consulta de estado enviada");
-
+   let response 
 
     try {
-        const bl = await exec('./bash_scripts/generacion_tabla_nodos.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt)
-        console.log('bl', bl)
+        response = await exec('./bash_scripts/generacion_tabla_nodos.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt)
       } catch (e) {
         console.log(e)
 
         return res.status(422).json(e)
       }
-    
-    
-      return res.status(200).json({test: 'test'});
-    
-  //  res.end("yes");
+         
+      return response.stderr ? res.status(422).json({errorMessage: 'error'}) : res.status(200).json({status: 'ok'});
 });
 
 app.post('/form_inicio',async function(req,res){
@@ -101,74 +101,77 @@ app.post('/form_inicio',async function(req,res){
     console.log("Numero de identificación del muestreo: "+ req.body.nro_muestreo);
     console.log("Muestreo sincronizado: " + req.body.sync);
     
+    let response
+
     if (req.body.sync == "SI"){
         console.log("Muestreo SINCRONIZADO");
        // const initTime = moment().add(req.body.epoch_inicio, 'minutes').format('x')
        const initTime = moment().add(req.body.epoch_inicio, 'm').unix()
        try {
-        const bl = await exec('./bash_scripts/principal_sync.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt + ' ' + req.body.duracion_muestreo + ' ' + req.body.nro_muestreo + ' '+ initTime +' ');
-        return res.status(422);
-    } catch (e) {
+        response = await exec('./bash_scripts/principal_sync.sh' + ' ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt + ' ' + req.body.duracion_muestreo + ' ' + req.body.nro_muestreo + ' '+ initTime +' ');
+        } catch (e) {
         console.log(e)
+        return res.status(422).json({error: e});
+
       }
     }
     else if(req.body.sync == "NO"){
         console.log("Muestreo NO SINCRONIZADO");
 
         try {
-            const bl = await exec('./bash_scripts/principal_async.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt + ' '  + req.body.duracion_muestreo + ' ' + req.body.nro_muestreo + ' ');
+            response = await exec('./bash_scripts/principal_async.sh ' + ' '  + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt + ' '  + req.body.duracion_muestreo + ' ' + req.body.nro_muestreo + ' ');
         } catch (e) {
             console.log(e)
-            return res.status(422);
+            return res.status(422).json({error: e});
 
         }
     }   
-     return res.status(200);
+     return response.stderr ? res.status(422).json({errorMessage: 'error'}) : res.status(200).json({test: 'test medicion'});
 
 });
 
 
 app.post('/cancelar_muestreo',async function(req,res){
     console.log("Boton apretado: Cancelar muestreo");
-
+    let response
     try {
-        const bl = await exec('./bash_scripts/cancelar_muestreo.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt);
+        response = await exec('./bash_scripts/cancelar_muestreo.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt);
     } catch (e) {
         console.log(e)
-        return res.status(422);
+        return res.status(422).json({error: e});
+
 
     }
-    return res.status(200);
+    return response.stderr ? res.status(422).json({errorMessage: 'error'}) : res.status(200).json({status: 'ok'});
 
 });
 
 
 app.post('/reiniciar_nodos',async function(req,res){
     console.log("Boton apretado: Reiniciar Nodos");
-
+    let response
     try {
-        const bl = await exec('./bash_scripts/reiniciar_nodos.sh ' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt);
+        response = await exec('./bash_scripts/reiniciar_nodos.sh' + ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt);
     } catch (e) {
         console.log(e)
-        return res.status(422);
-
+        return res.status(422).json({error: e});
     }
-    return res.status(200);
+    return response.stderr ? res.status(422).json({errorMessage: 'error'}) : res.status(200).json({status: 'ok'});
 
 });
 
 
 app.post('/borrar_SD',async function(req,res){
     console.log("Boton apretado: Borrar los archivos de los nodos");
-
+    let response
     try {
-        const bl = await exec('./bash_scripts/borrar_SD.sh '+ ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt );
+        response = await exec('./bash_scripts/borrar_SD.sh '+ ip_mqtt_broker + ' ' + usuario_mqtt + ' ' + pass_mqtt );
     } catch (e) {
         console.log(e)
-        return res.status(422);
+        return res.status(422).json({error: e});
 
     }
-    return res.status(200);
+    return response.stderr ? res.status(422).json({errorMessage: 'error'}) : res.status(200).json({status: 'ok'});
 
 });
 
