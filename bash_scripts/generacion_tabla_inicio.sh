4e6ee706032389791e17a7f2ede2303923088f6a
Andres Seivane <tu@correo.com>
2023-07-16 21:50:58 -0300

    Move mosquitto_sub to nodos_inicio Use temporal file for mqtt messages Change var names and indentation

--- bash_scripts/generacion_tabla_inicio.sh
+++ bash_scripts/generacion_tabla_inicio.sh
@@ -9,33 +9,33 @@ nro_medicion=$4
 
 topic2="nodo/confirmacion"
 
-archivo1="/app/public/datos/mediciones/medicion_$nro_medicion/mensajes_mqtt.log"
-archivo2="/app/public/datos/mediciones/medicion_$nro_medicion/tabla_nodos_inicio.csv"
+mqtt_log="/app/public/datos/mediciones/medicion_$nro_medicion/mensajes_mqtt.log"
+csv="/app/public/datos/mediciones/medicion_$nro_medicion/tabla_nodos_inicio.csv"
+temp="/app/public/datos/mediciones/medicion_$nro_medicion/temp.txt"
+
+ts=$(date "+%Y/%m/%d %H:%M:%S") # Guardamos la fecha y hora actual en una variable.
+echo "Hora Actual: $ts"
 
 # Guardo las respuestas y genero la tabla de nodos al inicio
-while read value; do
-    
-  ts=$(date "+%Y/%m/%d %H:%M:%S") # Guardamos la fecha y hora actual en una variable.
+while IFS= read -r line; do    
 
-   echo "Hora Actual: $ts"
-
-  topic=`echo "$value" | awk '{print $1}'`
+  topic=`echo "$line" | awk '{print $1}'`
 
   if [[ $topic = $topic2 ]]; then
-  # guardar id y estado del nodo recibido
-  # PREGUNTAR SI EL NODO ESTA REPETIDO EN LA TABLA 
-  nodoid=`echo "$value" | awk -F' ' 'BEGIN {OFS=","} {$1=""; sub("^,", ""); print}'`
-  nodoid_shell=`echo "$value" | awk '{print $2 "," $3 "," $5}'`
-  echo "$nodoid" >> $archivo2   # guardamos datos en archivo
-  echo "$ts mensaje recibido: [$value]" >> $archivo1   # guardamos datos en archivo
-  echo "$ts Nodo identificado: $nodoid_shell"
+    # guardar id y estado del nodo recibido
+    # PREGUNTAR SI EL NODO ESTA REPETIDO EN LA TABLA 
+    nodoid=`echo "$line" | awk -F' ' 'BEGIN {OFS=","} {$1=""; sub("^,", ""); print}'`
+    nodoid_shell=`echo "$line" | awk '{print $2 "," $3 "," $4}'`
+    echo "$nodoid" >> $csv   # guardamos datos en archivo
+    echo "$ts mensaje recibido: [$line]" >> $mqtt_log   # guardamos datos en archivo
+    echo "$ts Nodo identificado: $nodoid_shell"
   else 
-  # mensaje no identificado
-  echo "$ts mensaje recibido no identificao!: [$value]" >> $archivo1   # guardamos datos en archivo
-  echo "$ts mensaje recibido no identificao!: [$value]"
+    # mensaje no identificado
+    echo "$ts mensaje recibido no identificao!: [$line]" >> $mqtt_log   # guardamos datos en archivo
+    echo "$ts mensaje recibido no identificao!: [$line]"
   fi
-sync=`echo "$value" | awk '{print $6}'`
 
-done < <(timeout 5s mosquitto_sub -t $topic2 -h $broker -p $port -v -u $usr -P $pass) # se pone el comando acá en vez de antes del while porque sino se pierde el valor de las variables dentro del bucle
+done < $temp # se pone el comando acá en vez de antes del while porque sino se pierde el valor de las variables dentro del bucle
+rm $temp
 echo "Fin de la consulta de estado"
 
