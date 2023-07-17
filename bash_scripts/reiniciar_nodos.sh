c63c7dbebfb1f264617fa0b5b7c695157c9508e1
Andres Seivane <tu@correo.com>
2023-07-16 23:33:43 -0300

    fix variable definition

--- bash_scripts/reiniciar_nodos.sh
+++ bash_scripts/reiniciar_nodos.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-ip_mqtt_broker = $1
-usuario_mqtt = $2
-pass_mqtt = $3
+ip_mqtt_broker=$1
+usuario_mqtt=$2
+pass_mqtt=$3
 
 echo "Reiniciando todos los nodos"
 mosquitto_pub -h $ip_mqtt_broker -t "control/reiniciar" -u $usuario_mqtt -P $pass_mqtt -m "0" 
